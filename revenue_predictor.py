7a260bb5618b69bdbfe173dfd0c5d640a188d55e
Hiran <101395290+hiran-pnr@users.noreply.github.com>
2022-07-23 16:18:32 +0530

    Update revenue_predictor.py

--- revenue_predictor.py
+++ revenue_predictor.py
@@ -11,8 +11,8 @@ st.set_page_config(
      layout="centered",
      initial_sidebar_state="collapsed",
      menu_items={
-         'Get Help': None,
-         'Report a bug': None,
+         'Get Help': 'hiransurendran505@gmail.com',
+         'Report a bug': 'hiransurendran505@gmail.com',
          'About': "A web app developed by B1T2(Feb) of ICT Academy of Kerala for project related purpose."
      }
  )
